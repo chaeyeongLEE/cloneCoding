6ce64d568a5afbf5689aa0fc200d2eb02543ba96
chaeyeongLEE <dh503401@naver.com>
2024-08-26 17:54:34 +0900

    gh page 빌드하기

---
+++ movie-app/src/App.js
@@ -0,0 +1,20 @@
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
+function App() {
+    return (
+        <BrowserRouter basename="cloneCoding">
+            <Routes>
+                <Route path="/movie/:id" element={<Detail />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+
+    );
+}
+
+export default App;
