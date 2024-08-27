abc41ba090f2d562043e30f7833198eb9eda9427
chaeyeongLEE <dh503401@naver.com>
2024-08-28 00:44:40 +0900

    로딩창 디자인 수정 완료

--- movie-app/src/components/Loading.js
+++ movie-app/src/components/Loading.js
@@ -14,16 +14,36 @@ const Div = styled.div`
     z-index: 9999;
     
     & span {
-        width: 40px;
-        height: 40px;
+        width: 48px;
+        height: 48px;
+        background: #FFF;
         border-radius: 50%;
-        border: 5px solid transparent;
-        border-top-color: #3498db;
-        animation: spin 1s linear infinite;
+        display: inline-block;
+        position: relative;
+        box-sizing: border-box;
+        animation: rotation 1s linear infinite;
 
-        @keyframes spin {
-            0% { transform: rotate(0deg); }
-            100% { transform: rotate(360deg); }
+        &::after {
+            content: '';
+            box-sizing: border-box;
+            position: absolute;
+            left: 6px;
+            top: 10px;
+            width: 12px;
+            height: 12px;
+            color: #FF3D00;
+            background: currentColor;
+            border-radius: 50%;
+            box-shadow: 25px 2px, 10px 22px;
+        }
+
+        @keyframes rotation {
+            0% {
+                transform: rotate(0deg);
+            }
+            100% {
+                transform: rotate(360deg);
+            }
         }
     }
 `;
